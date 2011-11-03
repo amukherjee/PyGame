278cce38acc5edf3bcf3d4cf476357eb500302a7
Aditya Mukherjee <aditya.s.mukherjee@gmail.com>
2011-11-03 00:54:36 -0400

    defined took damage and killed methods

--- tank.py
+++ tank.py
@@ -14,12 +14,27 @@ class Tank(object):
     else
       return "%s (DEAD)" % self.name
 
+
   def fire_at(self, enemy):
 
     if self.ammo >=1:
       self.ammo -=1
       print self.name, "fires on ", enemy.name
+      enemy.took_damage()
     else:
       print self.name, "is out of ammo !"
 
+
+  def took_damage(self):
+
+    self.armor -= 20
+    print self.name , " just got hit with a shell!"
+    if self.armor <= 0
+      self.killed()
+
+
+  def killed(self):
+
+    self.alive = False
+    print self.name, " is dead !"
 
