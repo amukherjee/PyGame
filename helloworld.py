229ba5f32dd42565fb851af02a88f53d5652aca2
Aditya Mukherjee <aditya.s.mukherjee@gmail.com>
2011-12-16 02:06:32 -0500

    scrolly message example

--- helloworld.py
+++ helloworld.py
@@ -1,32 +1,35 @@
 #!/usr/bin/python -tt
 
-background_image_file = 'sushiplate.jpg'
-mouse_image_filename = 'fugu.png'
-
 import pygame
 from pygame.locals import *
 from sys import exit
 
 pygame.init()
 
-size = [640, 480]
-screen = pygame.display.set_mode(size,0,32)
+SCREEN_SIZE = (640, 480)
+screen = pygame.display.set_mode(SCREEN_SIZE,0,32)
+message = " This is a demonstration of the scrolly messae script.  "
 pygame.display.set_caption('Hello World')
 
-background_image_filename = 'data/background.gif'
-mouse_image_filename = 'data/alien1.png'
+background_image_filename = 'data/noserape.jpg'
 background = pygame.image.load(background_image_filename).convert()
-mouse_cursor = pygame.image.load(mouse_image_filename).convert_alpha()
+font  = pygame.font.SysFont("arial",80);
+text_surface = font.render(message, True, (0, 0, 255))
+x = 0
+y = (SCREEN_SIZE[1] - text_surface.get_height())/2
+
+
 
 while True:
   for event in pygame.event.get():
     if event.type == QUIT:
       exit()
- 
+
   screen.blit(background, (0,0))
-  x, y = pygame.mouse.get_pos()
-  x -= mouse_cursor.get_width() / 2
-  y -= mouse_cursor.get_height() / 2
-  screen.blit(mouse_cursor, (x, y))
- 
+
+  x-= 2
+  if x < -text_surface.get_width():
+    x= 0
+  screen.blit(text_surface, (x, y))
+  screen.blit(text_surface, (x+text_surface.get_width(), y))
   pygame.display.update()
